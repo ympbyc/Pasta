2390ff14c08c1ae8b67251f1af64c8b692f81a2f
Minori Yamashita <ympbyc@gmail.com>
2013-01-19 22:57:39 +0900

    fix styling issues

--- sampleApp/Todo/js/appModel.js
+++ sampleApp/Todo/js/appModel.js
@@ -10,14 +10,14 @@ var appModel = {
       , curId: 0
     });
   }
-  , 'todo-add': function (send, state, memo) {
+, 'todo-add': function (send, state, memo) {
     var id = state.curId+1;
     send({curId:id, todos: state.todos.concat(TodoItem(memo, id, false))});
   }
-  , 'todo-remove': function (send, state, id) {
+, 'todo-remove': function (send, state, id) {
     send({todos: __.filter(function (it) { return it.id !== id })(state.todos)});
   }
-  , 'todo-stat-change': function (send, state, item) {
+, 'todo-stat-change': function (send, state, item) {
     send({
       todos: __.map(function (it) {
         if (it.id !== item.id) return it;
--- sampleApp/Todo/js/viewUpdateRule.js
+++ sampleApp/Todo/js/viewUpdateRule.js
@@ -1,7 +1,7 @@
 var updateRule = {
   id: function (UIAPI, state) {
   }
-  , todos: function (UIAPI, state) {
+, todos: function (UIAPI, state) {
     UIAPI.todoList.renderTodos(state.todos);
     UIAPI.todoList.renderCounter(state.todos.length);
   }
