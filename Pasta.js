e07c115c001f755edcd1565054f2165dd92c27dc
Minori Yamashita <ympbyc@gmail.com>
2013-02-01 21:10:14 +0900

    Add comment to enphasize actor-based ideas

--- Pasta.js
+++ Pasta.js
@@ -1,11 +1,15 @@
 /*
  * Pasta.js
+ *
+ * Actor-based MVC framework
+ *
  * 2013 Minori Yamashita <ympbyc@gmail.com>
  */
 
 var Pasta = (function () {
   var __ = require('./Fw');
 
+  //Hide informations that are not necessary for appRule actor, but for execution
   var mainloopGenerator = function (config) {
     return function (api, state, ev, ev_val) {
       var kont = function (patch) { api.modifyState(patch); };
@@ -13,18 +17,24 @@ var Pasta = (function () {
     };
   };
 
+
   var UIHandler = function (appModel, UIAPI, updateRule, parentUI) {
     var self = {};       //interface to the external world
     var appState = {};   //mutable application state
     var mainloop = mainloopGenerator(appModel);
-    
+
+    //`signal` function is given to UI actors on construction time, UI actors send messages to appRule actors
+    // through this function.
+    //`signal` have to be defined here because it accesses the closed mutable value `appState`
     var signal = function (ev_name, fn) {
       return function (e) {
         var val = fn ? fn(e) : e;
         mainloop(self, appState, ev_name, val);
       };
     };
-  
+
+    //Send messages to View actor.
+    //Each message has a pattern `.changedVal (hash_of_UI_actors, copy_of_patched_appState, old_value)`
     function autoUpdate (patch) {
       var tempState = __.merge(appState, patch);
       __.hashFold(patch, null, function (change, key) {
@@ -32,26 +42,30 @@ var Pasta = (function () {
         if (updateRule[key] !== undefined) updateRule[key](UIAPI, tempState, appState[key]);
       });
     }
-  
+
+    //initialize each view actors
+    //app.js should send this
     self.start = function () {
       __.hashFold(UIAPI, null, function (mdl, key) {
         if (UIAPI[key].initialize) UIAPI[key].initialize(parentUI, signal);
       });
       signal('start')();
     };
-  
+
+    //call `autoUpdate`. When done, merge the patch to the current appState
     self.modifyState = function (patch) {
       autoUpdate(patch);       //apply changes to UIs
       __._merge(appState, patch); //destructively update appState
     };
-  
+
     return self;
   };
 
-  return {
-    UIHandler: UIHandler
-  , __: __
-  };
+  //Pasta() is UIHandler
+
+  UIHandler.__ = __;
+  return UIHandler;
+
 }());
 
 module.exports = Pasta;
