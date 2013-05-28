17a47cb0458f0b8f83abf6e9c38b58ce97d28190
Minori Yamashita <ympbyc@gmail.com>
2013-05-29 00:18:03 +0900

    Make TodoMVC example more readable. Rm Varicon

--- examples/todo-pasta/js/ui.js
+++ examples/todo-pasta/js/ui.js
@@ -1,78 +1,89 @@
 (function( window ) {
-  'use strict';
+    'use strict';
 
-  window.PastaTodo = window.PastaTodo || {};
+    window.PastaTodo = window.PastaTodo || {};
 
-  var TODO_TEMPLATE = $("#todo-template").html();
+    var TODO_TEMPLATE = $("#todo-template").html();
 
-  /* UI
-   * UI helpers
-   */
-  PastaTodo.UI = _.module(
-    {},
+    /* UI
+     * UI helpers
+     */
+    PastaTodo.UI = _.module({}, render_todos, switch_filter, update_footer, toggle_visibility, todo_wrap, finish_edit);
+
+
 
     //refresh the entire list
     function render_todos (todos) {
-      var $els = _.map(todos, todo_wrap);
-      $("#todo-list").html(""); //clear
-      _.each($els, function ($el) {
-        $el.appendTo($("#todo-list"));
-      });
-    },
+        var $els = _.map(todos, todo_wrap);
+        $("#todo-list").html(""); //clear
+        _.each($els, function ($el) {
+            $el.appendTo($("#todo-list"));
+        });
+    }
+
+
 
     //bold, unbold
     function switch_filter (new_filter, old_filter) {
-      $("#" + old_filter).removeClass("selected");
-      $("#" + new_filter).addClass("selected");
-    },
+        $("#" + old_filter).removeClass("selected");
+        $("#" + new_filter).addClass("selected");
+    }
+
+
 
     //adjust numbers
     function update_footer (todos) {
-      var nums = _.countBy(todos, function (todo) {
-        return todo.completed ? "completed" : "active";
-      });
-      $("#todo-count strong").text(nums.active || 0);
-      if (nums.completed)
-        $("#clear-completed").show().text("Clear completed (" + nums.completed + ")");
-      else
-        $("#clear-completed").hide();
-    },
+        var nums = _.countBy(todos, function (todo) {
+            return todo.completed ? "completed" : "active";
+        });
+        $("#todo-count strong").text(nums.active || 0);
+        if (nums.completed)
+            $("#clear-completed").show().text("Clear completed (" + nums.completed + ")");
+        else
+            $("#clear-completed").hide();
+    }
+
+
 
     //#main and #footer have to be hidden when no todo is available
     function toggle_visibility (v) {
-      $("#main,#footer").toggle(!!v);
+        $("#main,#footer").toggle(!!v);
     }
-  );
 
 
-  function todo_wrap (todo) {
-    var $el = $("<div>").html(_.template(TODO_TEMPLATE, todo)).find("li");
 
-    $el.find("label").dblclick(function () {
-      $el.addClass("editing");
-      $el.find(".edit").focus();
-    });
-    $el.find(".edit").keyup(function (e) {
-      if (PastaTodo.Helper.enter_key(e.which)) finish_edit($(this), $el, todo);
-    });
-    $el.find(".edit").blur(function (e) {
-      finish_edit($(this), $el, todo);
-    });
-    $el.find(".toggle").click(PastaTodo.pasta_signal("toggle_status", function () {
-      return { completed: $(this).is(":checked"), todo: todo };
-    }));
-    $el.find(".destroy").click(PastaTodo.pasta_signal("destroy_todo", function () {
-      return todo;
-    }));
+    function todo_wrap (todo) {
+        var $el = $("<div>").html(_.template(TODO_TEMPLATE, todo)).find("li");
 
-    return $el;
-  }
+        $el.find("label").dblclick(function () {
+            $el.addClass("editing");
+            $el.find(".edit").focus();
+        });
+        $el.find(".edit").keyup(function (e) {
+            if (PastaTodo.Helper.enter_key(e.which)) finish_edit($(this), $el, todo);
+        });
+        $el.find(".edit").blur(function (e) {
+            finish_edit($(this), $el, todo);
+        });
+        $el.find(".toggle").click(PastaTodo.pasta_signal("toggle_status", function () {
+            return { completed: $(this).is(":checked"), todo: todo };
+        }));
+        $el.find(".destroy").click(PastaTodo.pasta_signal("destroy_todo", function () {
+            return todo;
+        }));
 
-  function finish_edit ($this, $el, todo) {
-    $el.removeClass("editing");
-    var title = $this.val();
-    if (title) PastaTodo.pasta_signal("update")({ todo: todo, title: title });
-    else PastaTodo.pasta_signal("destroy_todo")(todo);
-  }
+        return $el;
+    }
+
+
+
+    function finish_edit ($this, $el, todo) {
+        $el.removeClass("editing");
+        var title = $this.val();
+        if (title) PastaTodo.pasta_signal("update")({ todo: todo, title: title });
+        else PastaTodo.pasta_signal("destroy_todo")(todo);
+    }
+
+
 
 })( window );
--- examples/todo-pasta/js/view.js
+++ examples/todo-pasta/js/view.js
@@ -1,25 +1,28 @@
 (function( window ) {
-  'use strict';
+    'use strict';
 
-  window.PastaTodo = window.PastaTodo || {};
+    window.PastaTodo = window.PastaTodo || {};
 
-  /* View
-   * View is a set of functions that maps the changes made to the app to its UI
-   */
-  PastaTodo.View = _.module(
-    {},
+    /* View
+     * View is a set of functions that maps the changes made to the app to its UI
+     */
+    PastaTodo.View = _.module({}, todos, show_mode);
+
+
 
     function todos (UI, app) {
-      UI.render_todos(PastaTodo.Helper[app.show_mode](app.todos));
-      UI.update_footer(app.todos);
-      UI.toggle_visibility(_.size(app.todos));
-      PastaTodo.pasta_signal("save_app")(); //save the app everytime todos change
-    },
+        UI.render_todos(PastaTodo.Helper[app.show_mode](app.todos));
+        UI.update_footer(app.todos);
+        UI.toggle_visibility(_.size(app.todos));
+        PastaTodo.pasta_signal("save_app")(); //save the app everytime todos change
+    }
+
+
 
     function show_mode (UI, app, old_val) {
-      UI.render_todos(PastaTodo.Helper[app.show_mode](app.todos));
-      UI.switch_filter(app.show_mode, old_val);
+        UI.render_todos(PastaTodo.Helper[app.show_mode](app.todos));
+        UI.switch_filter(app.show_mode, old_val);
     }
-  );
+
 
 })( window );
